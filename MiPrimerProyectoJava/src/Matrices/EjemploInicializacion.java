f0e12546f2371cffc5ec183e9634d771e131c82e
jhinwinson <josorioc@ufpso.edu.co>
2025-04-01 07:09:08 -0500

    Introduccion a Matrices

---
+++ MiPrimerProyectoJava/src/Matrices/EjemploInicializacion.java
@@ -0,0 +1,11 @@
+package Matrices;
+public class EjemploInicializacion {
+
+    public static void main(String[] args) {
+        int[][] matriz = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12}
+        };
+    }
+}
